0473927e3aaf8d674c06a24fa593045ee18f41d1
Sebastian <sebastian.rodriguezro@alumnos.uv.cl>
2023-11-24 13:53:16 -0300

    a

--- src/routes/userSQL.js
+++ src/routes/userSQL.js
@@ -2,7 +2,7 @@
 defines two routes: */
 import { Router } from 'express';
 
-import { getUsuarios, setUsuario, login, nombreYedad,guias,descripciones,usuariosComentariosRurales,usuariosSinComentarios,rutasAntes2022,conteoRutas,promedioEdadMontañosas,guiasSinRutasRurales, rutasMontañosas2Comentarios,} from '../controllers/sql/user.js';
+import {getUsuarios, setUsuario, changePassword, login2, nombreYedad,guias,descripciones,usuariosComentariosRurales,usuariosSinComentarios,rutasAntes2022,conteoRutas,promedioEdadMontañosas,guiasSinRutasRurales, rutasMontañosas2Comentarios, publicarRuta, RealizarRuta} from '../controllers/sql/user.js';
 
 
 const router = Router();
@@ -12,6 +12,20 @@ router.route('/ingresar')
 
 router.route('/mostrar')
     .get(getUsuarios);
+
+
+router.route('/login2')
+    .post(login2);
+
+router.route('/publicarRuta')
+    .post(publicarRuta);
+
+router.route('/RealizarRuta')
+    .post(RealizarRuta);
+
+router.route('/changePassword')
+    .post(changePassword);
+
 
 router.route('/userSin')
     .get(usuariosSinComentarios);
@@ -32,16 +46,16 @@ router.route('/nombreYedad')
 router.route('/antes2022rutas')
     .get(rutasAntes2022);
 
-router.route('/usuariosComentariosRurales')
+router.route('/error1')
     .get(usuariosComentariosRurales);
 
-router.route('/promedioEdadMontañosas')
+router.route('/promedioEdadMontanosas')
     .get(promedioEdadMontañosas);
 
 router.route('/guiasSinRutasRurales')
     .get(guiasSinRutasRurales);
 
-router.route('/rutasMontañosas2Comentarios')
+router.route('/rutasMontanosas2Comentarios')
     .get(rutasMontañosas2Comentarios);
 
 
